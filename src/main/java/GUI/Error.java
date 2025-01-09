21b88b3f5d6cfcb5f4e91c119c9305f4b76e1fcf
Yū <artembezzzz@gmail.com>
2025-01-09 16:28:25 +0100

    Rewrote error handling for proper debugging

--- src/main/java/GUI/Error.java
+++ src/main/java/GUI/Error.java
@@ -12,11 +12,21 @@ public class Error {
         JOptionPane.showMessageDialog(frame, msg, "Error", JOptionPane.ERROR_MESSAGE);
     }
 
-    Error(Exception e) {
+    Error(Throwable e) {
+        if (e.getCause().getCause() == null) {
+            e = e.getCause();
+        } else if (e.getCause() != null){
+            e = e.getCause().getCause();
+        }
         JOptionPane.showMessageDialog(null, e.getMessage(), e.getClass().getName(), JOptionPane.ERROR_MESSAGE);
     }
 
-    Error(Exception e, Component frame) {
+    Error(Throwable e, Component frame) {
+        if (e.getCause().getCause() == null) {
+            e = e.getCause();
+        } else if (e.getCause() != null){
+            e = e.getCause().getCause();
+        }
         JOptionPane.showMessageDialog(frame, e.getMessage(), e.getClass().getName(), JOptionPane.ERROR_MESSAGE);
     }
 }
